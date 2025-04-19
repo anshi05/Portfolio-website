2e46ff9f8187d81f80c471b2b4e5cdd00b44c7e1
anshi05 <anshijio123@gmail.com>
2025-04-19 22:18:43 +0530

    Blog: Add blog component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,6 +53,14 @@ const Navbar = () => {
 			),
 		},
 		{
+			path: "/blog",
+			name: (
+				<div className="flex items-center gap-2">
+					<ion-icon name="newspaper-outline"></ion-icon> Blog
+				</div>
+			),
+		},
+		{
 			path: "/contact",
 			name: (
 				<div className="flex items-center gap-2">
